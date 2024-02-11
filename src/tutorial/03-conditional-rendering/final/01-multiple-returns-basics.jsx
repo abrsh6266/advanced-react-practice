56726b3c549c80d2bae18fe636cde4868324946a
Abrham <abrsh6266@gmail.com>
2024-02-11 09:01:12 -0900

    conditional rendering

--- src/tutorial/03-conditional-rendering/final/01-multiple-returns-basics.jsx
+++ src/tutorial/03-conditional-rendering/final/01-multiple-returns-basics.jsx
@@ -1,17 +1,14 @@
 import { useEffect, useState } from 'react';
 
 const MultipleReturnsBasics = () => {
-  // while fetching data
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     setTimeout(() => {
-      // done fetching data
       setIsLoading(false);
     }, 3000);
   }, []);
 
-  // can return entire app
   if (isLoading) {
     return <h2>Loading...</h2>;
   }
