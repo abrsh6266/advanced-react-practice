28cf398d3d39e9a2df6c7376f530a71c057dd90b
Abrham <abrsh6266@gmail.com>
2024-02-13 03:23:22 -0900

    context api

---
+++ src/tutorial/09-context-api/starter/Navbar.jsx
@@ -0,0 +1,18 @@
+import { createContext, useState } from "react";
+import NavLinks from "./NavLinks";
+export const context = createContext();
+const Navbar = () => {
+  const [user, setUser] = useState({ name: "bob" });
+  const logout = () => {
+    setUser(null);
+  };
+  return (
+    <context.Provider value={{user,logout}}>
+      <nav className="navbar" >
+        <h5>CONTEXT API</h5>
+        <NavLinks/>
+      </nav>
+    </context.Provider>
+  );
+};
+export default Navbar;
---
+++ src/tutorial/09-context-api/starter/UserContainer.jsx
@@ -0,0 +1,21 @@
+import { useContext } from "react";
+import { context } from "./Navbar";
+
+const UserContainer = () => {
+  const {user,logout} = useContext(context)
+  return (
+    <div className='user-container'>
+      {user ? (
+        <>
+          <p>Hello There, {user.name.toUpperCase()}</p>
+          <button type='button' className='btn' onClick={logout}>
+            logout
+          </button>
+        </>
+      ) : (
+        <p>Please Login</p>
+      )}
+    </div>
+  );
+};
+export default UserContainer;
